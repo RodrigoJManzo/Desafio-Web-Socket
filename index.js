12a416825b12e2d8dda857e76424aed28c3e87bc
RodrigoManzo <rodrigoj.manzo@gmail.com>
2022-10-26 00:27:16 -0300

    arreglo los mensajes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {Server : HttpServer} = require ("http")
 const {Server : SocketIOServer} = require ('socket.io')
 
 const productos = require (`./models/producto/modelProducto`)
+const mensajes = require (`./models/Mensaje/modelMensaje`)
 
 const app = express ()
 const httpServer = new HttpServer(app)
@@ -15,11 +16,17 @@ httpServer.listen(PORT, ()=> console.log(`server corriendo en ${PORT}`))
 
 io.on(`connection`, socket =>{
     sendProductos(socket)
+    sendMensajes(socket)
     socket.on(`productoNuevo`, newProducto =>{
         productSaver(newProducto)
     })
+    socket.on(`mensajeNuevo`, mensajeNuevo=>{
+        messageSaver(mensajeNuevo)
+    })
 })
 
+
+//PRODUCTOS
 
 const sendProductos = async (socket)=>{
     const allProducts = await productos.getAll()
@@ -32,3 +39,17 @@ const productSaver = async (newProducto)=>{
     const allProducts = await productos.getAll()
     io.sockets.emit(`todosProductos`, allProducts)
 }
+
+
+//CHAT
+
+const sendMensajes = async (socket)=>{
+    const allMsg = await mensajes.getAll()
+    socket.emit(`todosMensajes`, allMsg)
+}
+
+
+const messageSaver = async (newMensaje)=>{
+    await mensajes.save(newMensaje)
+    const allMsg = await mensajes.getAll()
+    io.sockets.emit(`todosMensajes`, allMsg)}
--- public/assets/cliente.js
+++ public/assets/cliente.js
@@ -1,8 +1,15 @@
 const socket = io("http://localhost:8080")
 
-
+//Productos
 const formProductos = document.getElementById(`formProductos`)
 const displayProductos = document.getElementById(`productDisplay`)
+
+//Chat 
+
+const chatForm = document.getElementById(`chat`)
+const chatContainer = document.getElementById(`chatContainer`)
+
+// PRODUCTOS
 
 formProductos.addEventListener('submit', (e) =>{
     e.preventDefault()
@@ -25,4 +32,28 @@ const muestraProductos = async (products) =>{
     const compiled = Handlebars.compile(template)
     const html = compiled({products})
     displayProductos.innerHTML = html
+}
+
+
+//CHAT
+
+chatForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const data = new FormData(chatForm)
+    const values = Object.fromEntries(data)
+    chatForm.reset()
+    socket.emit(`mensajeNuevo`, values)
+    console.log(`Datos de Productos Recibidos`, values)
+} )
+
+socket.on(`todosMensajes`, allMsg =>{
+    muestraMensajes(allMsg)
+})
+
+const muestraMensajes = async (messages) =>{
+    const res = await fetch (`./../assets/templates/chatTemplate.handlebars`)
+    const template = await res.text()
+    const compiled = Handlebars.compile(template)
+    const html = compiled({messages})
+    chatContainer.innerHTML = html
 }
