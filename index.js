f56c8fd5088027af0d9e162fbab0b42f5511a597
RodrigoManzo <rodrigoj.manzo@gmail.com>
2022-10-25 23:21:48 -0300

    vuelvo a crear todo el handling de productos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const {Server : HttpServer} = require ("http")
 const {Server : SocketIOServer} = require ('socket.io')
 
-const products = require (`./models/producto/modelProducto`)
+const productos = require (`./models/producto/modelProducto`)
 
 const app = express ()
 const httpServer = new HttpServer(app)
@@ -15,6 +15,10 @@ httpServer.listen(PORT, ()=> console.log(`server corriendo en ${PORT}`))
 
 io.on(`connection`, socket =>{
     sendProductos(socket)
+
+    socket.on(`productoNuevo`, (newProducto) =>{
+        productSaver(newProducto)
+    })
 })
 
 
@@ -25,6 +29,14 @@ io.on(`connection`, socket =>{
 
 
 const sendProductos = async (socket)=>{
-    const allProducts = await products.getAll()
+    const allProducts = await productos.getAll()
     socket.emit(`todosProductos`, allProducts)
+}
+
+
+
+const productSaver = async (newProducto)=>{
+    await productos.save(newProducto)
+    const allProducts = await productos.getAll()
+    io.sockets.emit(`todosProductos`, allProducts)
 }
--- public/assets/cliente.js
+++ public/assets/cliente.js
@@ -1,9 +1,19 @@
 const socket = io("http://localhost:8080")
 
 
+const formProductos = document.getElementById(`formProductos`)
 
+formProductos.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const data = new FormData(formProductos)
+    const values = Object.fromEntries(data)
+    formProductos.reset()
+    socket.emit(`productoNuevo`, values)
+    console.log(`Datos de Productos Recibidos`, values)
+} )
 
 
 socket.on(`todosProductos`, data =>{
     console.log(data)
 })
+
